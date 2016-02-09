363c6761374cb585cde82b78f26cea9a4e6dfb16
hgs <hgs.dev@gmail.com>
2016-02-09 10:54:24 +0900

    return True in on_connect
    wait for release

--- eaecm/__init__.py
+++ eaecm/__init__.py
@@ -5,9 +5,11 @@ import nfc
 class MyCardReader(object):
 
     def on_connect(self, tag):
+        print "touched"
         self.idm = binascii.hexlify(tag.idm)
+        return True
 
-    def read_id(self):
+    def waiting_read_id(self):
         clf = nfc.ContactlessFrontend('usb')
         try:
             clf.connect(rdwr={'on-connect': self.on_connect})
@@ -18,5 +20,6 @@ if __name__ == '__main__':
     cr = MyCardReader()
     while True:
         print "touch card:"
-        cr.read_id()
+        cr.waiting_read_id()
+        print "released"
         print cr.idm
