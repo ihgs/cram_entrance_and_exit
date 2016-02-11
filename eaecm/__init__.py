e7f5a70271dd1c7ee334054351087a0f1600992a
hgs <hgs.dev@gmail.com>
2016-02-11 12:46:52 +0900

    send id to web server

--- eaecm/__init__.py
+++ eaecm/__init__.py
@@ -1,8 +1,14 @@
 import binascii
 import nfc
-
+import requests
+import time
+import datetime
 
 class MyCardReader(object):
+
+
+    def __init__(self, hostname, port):
+        self.url = "http://%s:%s/api/stamp" % (hostname, port)
 
     def on_connect(self, tag):
         print "touched"
@@ -16,10 +22,31 @@ class MyCardReader(object):
         finally:
             clf.close()
 
+    def send_id(self, id, reader="raspberry"):
+        now = datetime.datetime.now()
+        unix_time = int(time.mktime(now.timetuple()))
+        params = {
+            "time": unix_time,
+            "card_id": id,
+            "device_name": reader
+        }
+        response = requests.post(
+            self.url, params
+        )
+        if response.status_code == 200:
+            body = response.json()
+            if body["status"] == "success":
+                name = body["name"]
+                return name
+            else:
+                pass #TODO error
+        return None
+
 if __name__ == '__main__':
-    cr = MyCardReader()
-    while True:
-        print "touch card:"
-        cr.waiting_read_id()
-        print "released"
-        print cr.idm
+    cr = MyCardReader("localhost", "3000")
+    print cr.send_id("aaaaa")
+    # while True:
+    #     print "touch card:"
+    #     cr.waiting_read_id()
+    #     print "released"
+    #     print cr.idm
